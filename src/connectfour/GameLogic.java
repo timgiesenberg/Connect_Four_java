5c3f54552d5485b0405707d8aab00dcbd672b982
Sebastian Gohres <sebastian.gohres@gmail.com>
2014-04-20 18:15:34 +0200

    efficient diagonal line check

--- src/connectfour/GameLogic.java
+++ src/connectfour/GameLogic.java
@@ -2,15 +2,32 @@ package connectfour;
 
 import java.lang.Math.*;
 
+
+
 /**
  *
  * @author tim.giesenberg@me.com
  */
 public class GameLogic {
+
+    int[][] gamefield = new int[7][7];
+
     
     public GameLogic(){
+        check();
+    }
+
+    
+    private void check() {
+        for(int zeile = 0; zeile < gamefield.length; zeile++) {
+            for(int spalte = Math.max((3-zeile),0); spalte <= Math.min(9-zeile,(gamefield[zeile].length)-1); spalte++) {
+                System.out.println("Pruefe "+zeile+"/"+spalte);
+            }
+        }
+        
         
     }
+    
     
     public int getComputerMove(){
         double computerMoveDouble = Math.round(Math.random() * 7);
