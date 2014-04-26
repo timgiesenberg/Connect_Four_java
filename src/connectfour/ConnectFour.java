81c811a8549bc8461530432db340f9458390ef27
Sebastian Gohres <sebastian.gohres@gmail.com>
2014-04-26 15:14:41 +0200

    Some minor changes

--- src/connectfour/ConnectFour.java
+++ src/connectfour/ConnectFour.java
@@ -8,10 +8,20 @@ package connectfour;
 
 import java.util.ArrayList;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.MenuItemBuilder;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -24,163 +34,155 @@ import javafx.stage.Stage;
  *
  * @author tim.giesenberg@me.com
  */
-
-
-
-
-
 public class ConnectFour extends Application {
     
-
+    Group group;
+    Group dotGroup;
+    AnchorPane root;
+    Scene scene;
+    int xLines;
+    int yLines;
+    int cellWidth;
+    
+    private void centerGrid() {
+	MenuBar bar = (MenuBar) scene.lookup("#menubar");
+	int bH = (int) bar.getHeight();
+	int X = (int)Math.floor((root.getWidth() - (xLines*cellWidth))/2);
+	int Y = (int)Math.floor(((root.getHeight() - (yLines*cellWidth))/2)+Math.floor(bH/2));
+	
+	if(Y < bH) Y = bH;
+	group.setLayoutX(X);
+	group.setLayoutY(Y);
+	
+	
+	//if(stage.getWidth() < stage.getMinWidth()) stage.setWidth(stage.getMinWidth());
+	//if(stage.getHeight() < stage.getMinHeight()) stage.setHeight(stage.getMinHeight());
+	
+	
+    }
+    
     @Override
     public void start(Stage stage) throws Exception {
         
         GameLogic g = new GameLogic();
         g.getComputerMove();
         
-        AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-	Scene scene = new Scene(root);
+        root = (AnchorPane)FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+	stage.setMinHeight(200);
+	stage.setMinWidth(200);
+	stage.setWidth(600);
+	stage.setHeight(600);
+	stage.setMaxHeight(1000);
+	stage.setMaxWidth(1000);
+	root.setMinSize(200, 200);
+	root.setMaxSize(1000, 1000);
+	root.setPrefSize(600, 600);
+	scene = new Scene(root);
+	
+	
 
+	MenuBar menuBar = (MenuBar) scene.lookup("#menubar");
+	Node menu;
+	menu = scene.lookup("#mGame");
+	MenuItem aboutMenuItem =
+            MenuItemBuilder.create()
+                           .text("Close")
+                           .onAction(
+                               new EventHandler<ActionEvent>()
+                               {
+                                  @Override public void handle(ActionEvent e)
+                                  {
+                                     System.exit(0);
+                                  }
+                               })
+                           .accelerator(
+                               new KeyCodeCombination(
+                                  KeyCode.Q, KeyCombination.CONTROL_DOWN))
+                           .build();             
+         menuBar.getMenus().get(0).getItems().add(aboutMenuItem);
+	
+	 
+	 
+	stage.setScene(scene);
+	
+        stage.show(); 
+	
+	
+	
+	//rect2.setStroke(Color.RED);
 	int[][] gg = g.getGameGrid();
 	
 	int h;
 	int w;
 	int linelengthX, linelengthY;
-	int cellWidth = 50;
-	int marginX = 100;
-	int marginY = 100;
-	int xLines = gg[0].length;
-	int yLines = gg.length;
-	Line gridline;
+	cellWidth = 50;
 	
+	int marginY = 0;
+	xLines = gg[0].length;
+	yLines = gg.length;
+	int marginX = 0; //(int)Math.floor((root.getWidth() - (xLines*cellWidth))/2);
+	
+	
+
+	Line gridline;
+	group = new Group();
+	group.setLayoutX(0);
+	group.setLayoutY(0);
+	dotGroup = new Group();
+	
+	Rectangle rect = (Rectangle) scene.lookup("#recta");
+	rect.setFill(Color.DODGERBLUE);
+	group.getChildren().add(rect); 
+
 	
 	int useGridType = 1;
 	
 	// GRID TYPE 1
 	
-	
-/*
-	addEventFilter(MouseEvent.MOUSE_CLICKED,
-                new CFEventHandler<MouseEvent>() {
-                    public void handle(MouseEvent me) {
-                        getClickedCol(me);
-                    }
-                }*/
-	
 	if(useGridType == 1) {
-	    Rectangle rect = new Rectangle(marginX, marginY, xLines*cellWidth, yLines*cellWidth);
-	    
+	    //Rectangle rect = new Rectangle(marginX, marginY, xLines*cellWidth, yLines*cellWidth);
+	    rect.setLayoutX(0);
+	    rect.setLayoutY(0);
+	    rect.setWidth(xLines*cellWidth);
+	    rect.setHeight(yLines*cellWidth);
 	    rect.setId("clickarea");
-	    rect.setOnMouseClicked((EventHandler<? super MouseEvent>) new CFEventHandler());
-
-       
-	    
-	    root.getChildren().add(rect);
+	    CFEventHandler eh = new CFEventHandler(this);
+	    eh.gl = g;
+	    rect.setOnMouseClicked((EventHandler<? super MouseEvent>) eh);
+	    //root.getChildren().add(rect);
 	}
-	
-	
+		
 	// GRID TYPE 2
 	if(useGridType == 2 || useGridType == 1) {
 	    Color gridColor = (useGridType == 1) ? Color.BLUE : Color.BLACK;
 	    
 	    for(int i = 0; i <= Math.max(xLines, yLines); i++) {
-		h = marginY+(i*cellWidth);
-		w = marginX+(i*cellWidth);
+		h = (i*cellWidth);
+		w = (i*cellWidth);
 
 		if(i <= yLines) {
-		    linelengthX = (cellWidth*gg[0].length)+marginX;
-		    gridline = new Line(marginX, h, linelengthX, h);
+		    linelengthX = (cellWidth*gg[0].length);
+		    gridline = new Line(0, h, linelengthX, h);
 		    gridline.setStroke(gridColor);
 		    gridline.setSmooth(false);
 		    gridline.setStrokeWidth(2);
-		    root.getChildren().add(gridline);
-	
+		    //root.getChildren().add(gridline);
+		    group.getChildren().add(gridline);
 		}
 
 		if(i <= xLines) {
-		    linelengthY = (cellWidth*gg.length)+marginY;
-		    gridline = new Line(w, marginY, w, linelengthY);
+		    linelengthY = (cellWidth*gg.length);
+		    gridline = new Line(w, 0, w, linelengthY);
 		    gridline.setStroke(gridColor);
 		    gridline.setSmooth(false);
 		    gridline.setStrokeWidth(2);
-		    root.getChildren().add(gridline);    
+		    //root.getChildren().add(gridline);    
+		    group.getChildren().add(gridline);
 		}
 	    }
 	}
 	
 	
-
-	
-	
-	
-	
-	
-	/*
-	
-	final Canvas canvas = new Canvas(500,500);
-	GraphicsContext gc = canvas.getGraphicsContext2D();
-
-	
-	gc.setLineWidth(2);
-	gc.setStroke(Color.BLACK);
-
-	int h;
-	int w;
-	int linelength;
-	int cellWidth = 50;
-	int margin = 100;
-	for(int i = 0; i <= 7; i++) {
-	    h = margin+(i*cellWidth);
-	    w = margin+(i*cellWidth);
-	    linelength = (cellWidth*7)+margin;
-	    gc.strokeLine(margin, h, linelength, h);
-	    gc.strokeLine(w, margin, w, linelength);
-	}
-	
-	gc.fillOval(10, 10, 25, 25);
-	*/
-	
-	//
-	//gc.fillRect(10,10,210,10);
-
-	//root.getChildren().add(canvas);
-	
-	stage.setScene(scene);
-        stage.show(); 
-	
-	/*
-        Scene scene = new Scene(root);
-
-        scene.lookup("#gameFieldGrid").addEventFilter(MouseEvent.MOUSE_CLICKED,
-                new CFEventHandler<MouseEvent>() {
-                    public void handle(MouseEvent me) {
-                        getClickedCol(me);
-                    }
-                }
-        );
-        
-	
-	Node gf = scene.lookup("#gameFieldGrid");
-	
-	
-        ImageView iv1 = (ImageView) scene.lookup("#cell_0_1");
-        ImageView iv2 = (ImageView) scene.lookup("#cell_4_3");
-        
-        File oFile = new File("./src/img/o_yel.bmp");
-        File xFile = new File("./src/img/x_red.bmp");
-        
-        InputStream oIs = new BufferedInputStream(new FileInputStream(oFile.getAbsolutePath()));
-        InputStream xIs = new BufferedInputStream(new FileInputStream(xFile.getAbsolutePath()));
-        
-        Image o = new Image(oIs);
-        Image x = new Image(xIs);
-        
-        iv1.setImage(o);
-        iv2.setImage(x);
-        
-        System.out.println(iv2.getId());
-        */
-    
 	
 	
 	Test test = new Test(g.gamefield);
@@ -200,6 +202,10 @@ public class ConnectFour extends Application {
 	test.printGrid();
 	lines = g.check();
 	test.printResult(lines);
+	test.clearGrid();
+	
+	
+	
 	for(int i = 0; i < g.gamefield[0].length;i++) {
 	    System.out.println("Naechste freie Zeile in Spalte "+i+": "+g.getNextRow(i));
 	
@@ -211,10 +217,12 @@ public class ConnectFour extends Application {
 	Circle circle;
 	Color playerColor = Color.YELLOW;
 	System.out.println(gg.length + " "+ gg[0].length);
+	
+	
 	for(int y = 0; y < gg.length; y++) {
 	    for(int x  = 0; x < gg[y].length; x++) {
 		if(useGridType==1) {
-		    if(gg[y][x] == 0) playerColor = Color.LIGHTGREY;
+		    if(gg[y][x] == 0) playerColor = Color.web("f4f4f4");
 		} else {
 		    if(gg[y][x] == 0) continue;
 		}
@@ -224,37 +232,54 @@ public class ConnectFour extends Application {
 		} else if(gg[y][x] == 2) {
 		    playerColor = Color.BLUE;
 		}
-		centerX = (marginX+Math.floor(cellWidth/2))+(x*cellWidth);
-		centerY = marginY+Math.floor(cellWidth/2)+(y*cellWidth);
+		centerX = (Math.floor(cellWidth/2))+(x*cellWidth);
+		centerY = Math.floor(cellWidth/2)+(y*cellWidth);
 		 circle = new Circle(centerX,centerY,20,playerColor);
 		 circle.setStrokeWidth(2);
 		 circle.setMouseTransparent(true);
-		 root.getChildren().add(circle);
+		 
+		 dotGroup.getChildren().add(circle);
 	    }
 	}
+	group.getChildren().add(dotGroup);
+	root.getChildren().add(group); 
 	
+	scene.widthProperty().addListener(new ChangeListener<Number>() {
+	    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+		 centerGrid();
+		 
+	    }
+	});
 	
+	scene.heightProperty().addListener(new ChangeListener<Number>() {
+	    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+		 centerGrid();
+	    }
+	});
 	
-	
-        
+        centerGrid();
 
-	
-	
-	
 	//System.exit(0);
     }
-
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
+    
+    
+    public void drawToken(int row, int col, int player) {
+	double centerX, centerY;
+	Color c = (player == 1) ? Color.RED : Color.BLUE;
+		
+	//centerX = (Math.floor(cellWidth/2))+(col*cellWidth);
+	//centerY = Math.floor(cellWidth/2)+(row*cellWidth);
+	int index = row*xLines+col;
+	System.out.println("ROW: "+ row +  " COL: " + col + " Ylines: " + yLines + " Xlines: " + xLines + " Index: "+ index);
+	
+	Circle circle = (Circle) dotGroup.getChildren().get(index);
+	circle.setMouseTransparent(true);
+	circle.setFill(c);
+	
     }
+    
+
+
 
 
 
